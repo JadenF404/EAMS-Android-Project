cb9e95b649236c2a90826fd10f8d29f5246629e5
Aditya Baindur <adityabaindur@Adityas-MacBook-Air-433.local>
2024-10-07 13:48:19 -0400

    Fixed UI & Added Landing Page

---
+++ Deliverable_1_SEG/app/src/main/java/com/example/deliverable_1_seg/AttendeeLoginActivity.java
@@ -0,0 +1,12 @@
+package com.example.deliverable_1_seg;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AttendeeLoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.attendee_login_form); // Link to attandee_login_form.xml
+    }
+}
--- Deliverable_1_SEG/app/src/main/java/com/example/deliverable_1_seg/MainActivity.java
+++ Deliverable_1_SEG/app/src/main/java/com/example/deliverable_1_seg/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.deliverable_1_seg;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Set padding for system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Find the Attendee button and set its click listener
+        Button attendeeButton = findViewById(R.id.Attendee_button);
+        attendeeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch AttendeeLoginActivity when button is clicked
+                Intent intent = new Intent(MainActivity.this, AttendeeLoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
