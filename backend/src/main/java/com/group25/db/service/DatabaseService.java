3619a898f6e0aca29b2a1d35b99b2a3df364630f
millerding <mding022@uottawa.ca>
2024-10-11 13:52:43 -0400

    Implement the mapper into service layer

--- backend/src/main/java/com/group25/db/service/DatabaseService.java
+++ backend/src/main/java/com/group25/db/service/DatabaseService.java
@@ -1,5 +1,23 @@
 package com.group25.db.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.group25.db.model.Attendee;
+
+@Service
 public class DatabaseService {
-    
+    @Autowired
+    private AttendeeMapper attendeeMapper;
+
+    public boolean signUpUser(Attendee attendee) {
+        try {
+            attendeeMapper.insertUser(attendee);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
 }
