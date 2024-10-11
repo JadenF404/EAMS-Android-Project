5902c39cc0af8a2f632578258c0ce806874e09c8
millerding <mding022@uottawa.ca>
2024-10-11 13:06:49 -0400

    Initiate Spring Boot application boilerplate for database mapper

---
+++ backend/src/main/java/com/group25/db/EamsDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.group25.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EamsDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EamsDatabaseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/group25/db/EamsDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group25.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EamsDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
