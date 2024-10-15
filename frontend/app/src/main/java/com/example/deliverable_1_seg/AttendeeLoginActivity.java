d71a88b58f23793e4e6cccca2c3acd7dcd3e5a52
Jaden F <72635494+JadenF404@users.noreply.github.com>
2024-10-14 22:57:04 -0400

    added back button to attendee login screen

--- frontend/app/src/main/java/com/example/deliverable_1_seg/AttendeeLoginActivity.java
+++ frontend/app/src/main/java/com/example/deliverable_1_seg/AttendeeLoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.deliverable_1_seg;
 
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AttendeeLoginActivity extends AppCompatActivity {
@@ -8,5 +10,10 @@ public class AttendeeLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.attendee_login_form); // Link to attandee_login_form.xml
+    }
+
+    //Back button to go back to login page
+    public void backAttendeeForm (View view) {
+        finish();
     }
 }
