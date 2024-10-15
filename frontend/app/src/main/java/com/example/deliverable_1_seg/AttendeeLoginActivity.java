ec83b0482dc6c6364fa0bcb883d0f4cf8ba69ddf
Aditya Baindur <adityabaindur@Adityas-MacBook-Air-443.local>
2024-10-15 16:53:32 -0400

    Fixed UI & Added reset password field

--- frontend/app/src/main/java/com/example/deliverable_1_seg/AttendeeLoginActivity.java
+++ frontend/app/src/main/java/com/example/deliverable_1_seg/AttendeeLoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.deliverable_1_seg;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,11 +11,50 @@ public class AttendeeLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.attendee_login_form); // Link to attandee_login_form.xml
+        setContentView(R.layout.attendee_login_screen); // Link to attendee_login_form.xml
+
+
+        // Find the Attendee button and set its click listener
+        Button ForgotPassword_bttn = findViewById(R.id.ForgotPassword_bttn);
+
+        ForgotPassword_bttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch AttendeeLoginActivity when button is clicked
+                Intent intent = new Intent(AttendeeLoginActivity.this, AttendeePasswordChange.class);
+                startActivity(intent);
+            }
+
+        });
+
+
+
     }
+
+
+    // Method to switch to the signup form
+    public void signupAttendee(View view) {
+        setContentView(R.layout.attendee_signup_form); // Link to attendee_signup_form.xml
+    }
+
+    public void notanAttendee(View view){
+        finish();
+    }
+
+
+    public void login_attendee(View view){
+        //Todo MILLER: ADD THE CODE FOR ATTENDEE LOGIN HERE
+    }
+
+
+
+
+
 
     //Back button to go back to login page
     public void backAttendeeForm (View view) {
         finish();
     }
+
+
 }
--- frontend/app/src/main/java/com/example/deliverable_1_seg/OrganizerLoginActivity.java
+++ frontend/app/src/main/java/com/example/deliverable_1_seg/OrganizerLoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.deliverable_1_seg;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,8 +11,40 @@ public class OrganizerLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.organizer_login_form); // Link to attandee_login_form.xml
+        setContentView(R.layout.organizer_login_screen); // Link to attandee_login_form.xml
+
+
+        // Find the Attendee button and set its click listener
+        Button ForgotPassword_bttn = findViewById(R.id.ForgotPassword_bttn);
+
+        ForgotPassword_bttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch AttendeeLoginActivity when button is clicked
+                Intent intent = new Intent(OrganizerLoginActivity.this, OrganizerPasswordChange.class);
+                startActivity(intent);
+            }
+
+        });
     }
+
+
+
+
+
+
+    // Method to switch to the signup form
+    public void signupOrganizer(View view) {
+        setContentView(R.layout.organizer_signup_form); // Link to attendee_signup_form.xml
+    }
+    public void notAnOrganizer(View view){
+        finish();
+    }
+
+    public void org_attendee(View view){
+        //Todo MILLER: ADD THE CODE FOR ATTENDEE LOGIN HERE
+    }
+
 
     //Back button to go back to login page
     public void backOrganizerForm (View view) {
