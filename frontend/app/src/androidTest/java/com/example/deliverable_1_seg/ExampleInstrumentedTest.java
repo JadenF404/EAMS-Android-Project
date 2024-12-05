cdff3a664d0b7c7ac032867ab20c188fecd2b1de
Aditya <adityabaindur@icloud.com>
2024-12-05 00:38:51 -0500

    Added Video & Report

--- frontend/app/src/androidTest/java/com/example/deliverable_1_seg/ExampleInstrumentedTest.java
+++ frontend/app/src/androidTest/java/com/example/deliverable_1_seg/ExampleInstrumentedTest.java
@@ -5,7 +5,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.intent.Intents.intended;
+
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 
 import android.content.Intent;
